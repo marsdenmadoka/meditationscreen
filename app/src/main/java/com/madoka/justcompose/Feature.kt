89e39ba6ceca4c1266d3dcc72b3038a66e9ee6b9
marsdenmadoka <marsmadoka98@gmail.com>
2022-09-16 11:14:00 +0300

    added greetinsection ,chip section,current meditationsection

---
+++ app/src/main/java/com/madoka/justcompose/Feature.kt
@@ -0,0 +1,3 @@
+package com.madoka.justcompose
+
+data class Feature()
--- app/src/main/java/com/madoka/justcompose/ui/HomeScreen.kt
+++ app/src/main/java/com/madoka/justcompose/ui/HomeScreen.kt
@@ -1,7 +1,148 @@
 package com.madoka.justcompose.ui
 
-import androidx.compose.runtime.Composable
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.madoka.justcompose.R
+import com.madoka.mypokedexapp.ui.theme.*
 
 @Composable
 fun HomeScreen() {
+    Box(
+        modifier = Modifier
+            .background(TypeDragon)
+            .fillMaxSize()
+    ) {
+        Column {
+            GreetingsSection()
+            ChipSection(chips = listOf("insomia", "depression", "malaria", "insomia", "insomia"))
+            CurrentMeditation()
+        }
+    }
+}
+
+@Composable
+fun GreetingsSection(
+    name: String = "Marsden"
+) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp)
+    ) {
+        Column(verticalArrangement = Arrangement.Center) {
+
+            Text(
+                text = "Hello Morning, $name",
+                style = MaterialTheme.typography.h2 //font style
+            )
+            Text(
+                text = "We Wish you have a good day!",
+                style = MaterialTheme.typography.body1 //font style
+            )
+        }
+
+        Icon(
+            painter = painterResource(id = R.drawable.ic_search),
+            contentDescription = "Search",
+            tint = Color.White,
+            modifier = Modifier.size(24.dp)
+        )
+    }
+
+}
+
+
+@Composable
+fun ChipSection(
+    chips: List<String>
+) {
+    var selectedChipIndex by remember {
+        mutableStateOf(0)
+    }
+    //a row that is scrollable
+    LazyRow {
+        items(chips.size) { //how items we want to have
+            Box(
+                modifier = Modifier //chip
+                    .padding(start = 15.dp, top = 15.dp, bottom = 15.dp)
+                    .clickable {
+                        selectedChipIndex = it
+                    }
+                    .clip(RoundedCornerShape(10.dp))
+                    .background(
+                        if (selectedChipIndex == it) TypeWater //color
+                        else DarkerButtonBlue
+                    )
+                    .padding(15.dp)
+            ) {
+                Text(text = chips[it], color = TextWhite)
+            }
+        }
+    }
+}
+
+@Composable
+fun CurrentMeditation(color: Color = LightRed) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+            .padding(15.dp)
+            .clip(RoundedCornerShape(10.dp))
+            .background(color)
+            .padding(horizontal = 15.dp, vertical = 20.dp)
+            .fillMaxWidth()
+    ) {
+        Column {
+            Text(
+                text = "Daily Though",
+                style = MaterialTheme.typography.h2 //font style
+            )
+            Text(
+                text = "Meditation * 3-10 min",
+                style = MaterialTheme.typography.body1,//font style
+                color = TextWhite
+            )
+        }
+
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .background(ButtonBlue)
+                .padding(10.dp)
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_play),
+                contentDescription = "Play",
+                tint = Color.White,
+                modifier = Modifier.size(16.dp)
+            )
+        }
+
+    }
+}
+
+
+@Composable
+fun FeatureSection(){
+
 }
